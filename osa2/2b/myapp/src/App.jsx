68e931e40660ed9ca5be07130e8e5444cacf7cb7
Tuukka Yildirim <tuukka.yildirim@gmail.com>
2023-12-30 20:44:05 +0200

    2.6

---
+++ osa2/2b/myapp/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input onChange={(e) => {
+            setNewName(e.target.value)
+          }}/>
+        </div>
+        <div>
+          <button type="submit" onClick={(e) => {
+            e.preventDefault()
+            setPersons(persons.concat({ name: newName }))
+            setNewName('')
+          }}>add</button>
+        </div>
+      </form>
+      <div>debug: |{newName}|</div>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+      </ul>
+    </div>
+  )
+
+}
+
+export default App
