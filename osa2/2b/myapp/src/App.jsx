89ff3e84b6007f767aeb2b4e7890d22b67a8a5cf
Tuukka Yildirim <tuukka.yildirim@gmail.com>
2023-12-31 12:33:30 +0200

    2.9

--- osa2/2b/myapp/src/App.jsx
+++ osa2/2b/myapp/src/App.jsx
@@ -2,15 +2,26 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244' },
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterName, setFilterName] = useState('')
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
+        <div>
+          filter shown with <input onChange={(e) => {
+            setFilterName(e.target.value)
+          }} value={filterName}/>
+        </div>
+        <h2>add a new</h2>
         <div>
           name: <input onChange={(e) => {
             setNewName(e.target.value)
@@ -37,7 +48,7 @@ const App = () => {
       <div>debug: |{newName}|</div>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
+        {persons.filter(person => person.name.toLowerCase().startsWith(filterName.toLowerCase())).map(person => <li key={person.name}>{person.name} {person.number}</li>)}
       </ul>
     </div>
   )
